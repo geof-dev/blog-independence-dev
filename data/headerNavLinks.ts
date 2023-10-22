48944e5239bc87e8573aaebdc11d990f57d8211a
Geof - Independence DEV <contact@independence-dev.com>
2023-10-22 17:25:26 +0300

    first commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/formations', title: 'Formations' },
+  { href: '/a-propos', title: 'À propos' },
+]
+
+export default headerNavLinks
