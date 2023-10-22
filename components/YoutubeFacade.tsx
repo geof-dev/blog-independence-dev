dc18c2a8f2760edf1982cbcd36ac52e537983c9c
Geof - Independence DEV <contact@independence-dev.com>
2023-10-22 20:21:26 +0300

    Youtube Facade for speed page

--- components/YoutubeFacade.tsx
+++ components/YoutubeFacade.tsx
@@ -1,53 +1,57 @@
 'use client'
 
-import { useState } from 'react';
+import { useState } from 'react'
+import Image from 'next/image'
 
 const YoutubeFacade = ({ videoId, title }) => {
-    const [showVideo, setShowVideo] = useState(false);
+  const [showVideo, setShowVideo] = useState(false)
 
-    const handleClick = () => {
-        setShowVideo(true);
-    };
+  const handleClick = () => {
+    setShowVideo(true)
+  }
 
-    return (
-        <div className="relative">
-            {!showVideo && (
-                <div
-                    className="relative cursor-pointer"
-                    onClick={handleClick}
-                >
-                    {/* Display the video thumbnail here */}
-                    <img
-                        src={`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`}
-                        alt={title}
-                        className="w-full"
-                    />
-                    {/* YouTube logo overlay */}
-                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                        <img
-                            src="/static/images/youtube-logo.png" // Replace with the path to your YouTube logo image
-                            alt="YouTube Logo"
-                            className="w-24 h-16"
-                        />
-                    </div>
-                </div>
-            )}
-
-            {/* If showVideo is true, display the YouTube iframe */}
-            {showVideo && (
-                <div className="aspect-ratio-16/9">
-                    <iframe
-                        width="100%"
-                        height="400"
-                        src={`https://www.youtube.com/embed/${videoId}?&autoplay=1`}
-                        title={title}
-                        loading="lazy"
-                        allowFullScreen
-                    ></iframe>
-                </div>
-            )}
+  return (
+    <div className="relative">
+      {!showVideo && (
+        <button
+          className="relative cursor-pointer"
+          onClick={handleClick}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter' || e.key === 'Space') {
+              handleClick()
+            }
+          }}
+        >
+          <Image
+            src={`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`}
+            alt={title}
+            width={1280}
+            height={720}
+          />
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+            <Image
+              src={'/static/images/youtube-logo.png'}
+              alt={'YouTube Logo'}
+              width={100}
+              height={100}
+            />
+          </div>
+        </button>
+      )}
+      {showVideo && (
+        <div className="aspect-ratio-16/9">
+          <iframe
+            width="100%"
+            height="400"
+            src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+            title={title}
+            loading="lazy"
+            allowFullScreen
+          ></iframe>
         </div>
-    );
-};
+      )}
+    </div>
+  )
+}
 
-export default YoutubeFacade;
+export default YoutubeFacade
