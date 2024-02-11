f49ef462e0fd1feca71f6e9852fb384329e80128
Geof - Independence DEV <independence.dev.pro@gmail.com>
2024-02-11 20:53:41 +0200

    dev3 academy link

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: 'https://formations.independence.dev', title: 'Formations' },
+  { href: 'https://go.independence.dev/dev3-academy', title: 'Dev3 Academy' },
   //{ href: '/a-propos', title: 'À propos' },
 ]
 
